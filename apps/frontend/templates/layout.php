d05007c75be75b3915899df6ff43e516fee409c6
rickyos <ricardo_barronb@hotmail.com>
2014-07-03 19:06:06 -0400

    todo en español

--- apps/frontend/templates/layout.php
+++ apps/frontend/templates/layout.php
@@ -14,34 +14,35 @@
                 <div id="logo">
                     <div id="logo_text">
                         <!-- class="logo_colour", allows you to change the colour of the text -->
-                        <h1><a href="index.html">CSS3<span class="logo_colour">_seascape_two</span></a></h1>
-                        <h2>Simple. Contemporary. Website Template.</h2>
+                        <h1><a href="index.html">Navtur<span class="logo_colour"> - Bolivia</span></a></h1>
+                        <h2>Viajes en buque para Bolivia</h2>
                     </div>
                 </div>
                 <nav>
                     <ul class="sf-menu" id="nav">
-                        <li class="selected"><a href="index.html">Home</a></li>
-                        <li><a href="examples.html">Examples</a></li>
+                        <li class="selected"><a href="index.html">Inicio</a></li>
+<!--                        <li><a href="examples.html">Examples</a></li>
                         <li><a href="page.html">A Page</a></li>
-                        <li><a href="another_page.html">Another Page</a></li>
-                        <li><a href="#">Example Drop Down</a>
+                        <li><a href="another_page.html">Another Page</a></li>-->
+                        <li><a href="#">Nosotros</a>
                             <ul>
-                                <li><a href="#">Drop Down One</a></li>
-                                <li><a href="#">Drop Down Two</a>
+                                <li><a href="#">Mision y Vision</a></li>
+                                <li><a href="#">Viajes</a>
                                     <ul>
-                                        <li><a href="#">Sub Drop Down One</a></li>
-                                        <li><a href="#">Sub Drop Down Two</a></li>
-                                        <li><a href="#">Sub Drop Down Three</a></li>
-                                        <li><a href="#">Sub Drop Down Four</a></li>
-                                        <li><a href="#">Sub Drop Down Five</a></li>
+                                        <li><a href="#">Semana Santa</a></li>
+                                        <li><a href="#">Tiwanaku</a></li>
+                                        <li><a href="#">Copacabana</a></li>
+<!--                                        <li><a href="#">Sub Drop Down Four</a></li>
+                                        <li><a href="#">Sub Drop Down Five</a></li>-->
                                     </ul>
                                 </li>
-                                <li><a href="#">Drop Down Three</a></li>
+<!--                                <li><a href="#">Drop Down Three</a></li>
                                 <li><a href="#">Drop Down Four</a></li>
-                                <li><a href="#">Drop Down Five</a></li>
+                                <li><a href="#">Drop Down Five</a></li>-->
                             </ul>
                         </li>
-                        <li><a href="contact.php">Contact Us</a></li>
+                        <li><a href="contact.php">Contacto</a></li>
+                        <li><a href="contact.php">Noticias</a></li>
                     </ul>
                 </nav>
             </header>
