6a986c43ace20ec04755fae080b9ee57d5fc58e8
rickyos <ricardo_barronb@hotmail.com>
2014-09-11 19:11:40 -0400

    se arreglo las imagenes de cruceros y bordes de imagenes

--- apps/frontend/modules/home/templates/acercaDeSuccess.php
+++ apps/frontend/modules/home/templates/acercaDeSuccess.php
@@ -2,7 +2,7 @@
 <div class="article">
     <h2><span>Nuestra </span> Misión</h2>
     <div class="clr"></div>
-    
+
     <p align="justify"> “Adiestrar al personal de la armada boliviana en el arte de la navegación y faenas marineras, brindar asistencia a las poblaciones ribereñas del lago TITICACA en desastres naturales, apoyar a expediciones científicas y estudios del lago, promover el turismo en la región, a fin de contribuir al desarrollo de los interéses marítimos fluviales y lacustres en el lago TITICACA.”</p>
 
     <h2><span>Nuestra </span> Visión</h2>
@@ -15,35 +15,35 @@
     <h2><span>Reseña </span> Histórica</h2>
     <div class="clr"></div>
     <p align="justify"> El inicio del Proyecto de la “Construcción del Buque Escuela” se plasmó el año 2000 al mando del entonces CN. DEMN. Armando Pacheco Gutiérrez, bajo su cargo se encontraban 30 hombres entre ingenieros, técnicos navales y marineros con la firme intención de hacer realidad uno de los proyectos mas importantes de la entonces Fuerza Naval Boliviana.
-        <p align="justify"> La obra comenzó con la construcción de los cascos en el Astillero del Cuarto Distrito Naval “Titicaca” colocándose el Puente de Unión para después realizarse una botadura al Lago con la intención de verificar la correcta construcción estructural de los cascos.</p>
-        <p align="justify"> El año 2005 el proyecto vence un nuevo reto esta vez al mando del entonces CF. DIM. Marcelo García Apala , ya con los sistemas de propulsión instalados vuelve a botarse la embarcación procediéndose a las pruebas de maquinas y realizándose la primera travesía rumbo al Puerto de Guaqui, donde proseguirían los trabajos de construcción del salón, cubierta al aire libre y el puente de mando, esta decisión tomada por el mando naval, se debió a las características favorables de este puerto, aguas tranquilas y poco viento, ideales para terminar la obra.</p>
-        <p align="justify"> El año 2006 el CN. DIM. Marcelo García Apala logró cambiar el nombre del proyecto a “Construcción del Buque Multipropósito” con la visión de que el buque tendría que ser autosostenible y tener varias tareas de impacto socio cultural para fortalecer la imagen de la Institución Naval. Paralelamente el mismo CN. García impulsó la creación del Servicio de Industria Naval a partir del proyecto, pero con la intención de desarrollar la construcción de buques a nivel nacional.</p>
-        <div class="imagen_paquetes">
-            <img src="/images/us101.jpg" />
-        </div>
-        <p align="justify"> Durante las gestiones 2006 y 2007 por orden del entonces V. Almte. José Alba Arnés Comandante de la F.N.B. se paralizó el proyecto congelando la asignación de recursos  económicos y frustrando una aspiración institucional por un lapso de tiempo valioso.</p>
-        <p align="justify"> A mediados del año 2007 el Director de la obra CN. Marcelo García Apala con el apoyo de autoridades locales de Guaqui logró captar el interés del Presidente del Estado Evo Morales y se logra un apoyo económico de 411000 $U$ a través del programa “Evo Cumple”, con este presupuesto se decide de manera acertada reemplazar los motores propulsores adquiridos a medio uso por Motores Scania de ultima tecnología sueca y con el presupuesto restante se logra concluir la obra el 12 de febrero de 2009.</p>
-        <p align="justify"> A partir de la entrega de la obra asume el CC. CGON. Adalid Alfaro Palma como el Primer Comandante del Buque Multipropósito, quien fue componente importante del equipo de constructores en el área de propulsión y maquinas navales, con la misión de dar continuidad al proyecto en cuanto a detalles técnicos y de operacionalidad de la embarcación, además de lograr que la unidad sea autosostenible para no erogar gastos al Estado.</p>
-        <div class="imagen_acerc">
-            <img src="/images/us100.jpg" />
-        </div>
-        <p align="justify"> A partir de entonces el “Buque Multipropósito” empezó a desarrollar diversas tareas como el apoyo a la capacitación del personal de cuadros, cadetes, alumnos y marineros en el arte de la navegación, apoyo a Defensa Civil ante desastres naturales en poblaciones ribereñas, apoyo a las actividades de investigación científica para el cuidado del medio ambiente y arqueología subacuática y la tarea mas complicada, desarrollar el turismo en la región de Guaqui.   </p>
-        <p align="justify"> Durante la gestión 2010 el Buque Multipropósito con dependencia del Departamento III Operaciones, logró cumplir las tareas encomendadas y en el ámbito turístico solo en su primer año de funcionamiento acogió a más de 10000 turistas en su mayoría nacionales realizando cruceros a través del lago Titicaca contribuyendo de manera exitosa al desarrollo regional y logrando la autosostenibilidad del proyecto.  </p>
-        <div class="imagen_acerc">
-            <img src="/images/us102.jpg" />
-        </div>
-        <p align="justify"> A fines de 2010  el Buque Multipropósito se convierte en la Unidad Operativa de Servicios “NAVTUR” asumiendo como primer Comandante el CC. CGON. Adalid Alfaro Palma  con la misión de dar consecución a los logros obtenidos, teniendo a su cargo el Buque Multipropósito, tres embarcaciones menores, un Astillero de mantenimiento y dos oficinas de información en la ciudad de La Paz.</p>
-        <p align="justify"> El año 2013 pasa a depender de la Dirección General de Participación y Apoyo al Desarrollo conformándose las Unidades dependientes en base a las Unidades Operativas de Servicios como Transnaval, Industria Naval, Agropecuaria encarando actividades y proyectos orientados a desarrollar los II.MM.FF. y LL. en Bolivia. </p>
-        <p align="justify"> En la actualidad esta unidad tiene la proyección de convertirse en una unidad a nivel nacional con Buques Multipropósito y subunidades de apoyo en localidades estratégicas como Trinidad, Ribereralta, Guayaramerin, Quijarro, para de esta manera multiplicar los impactos económicos, socioculturales y educativos que realiza esta unidad en beneficio institucional y de la población boliviana.   </p>
+    <p align="justify"> La obra comenzó con la construcción de los cascos en el Astillero del Cuarto Distrito Naval “Titicaca” colocándose el Puente de Unión para después realizarse una botadura al Lago con la intención de verificar la correcta construcción estructural de los cascos.</p>
+    <p align="justify"> El año 2005 el proyecto vence un nuevo reto esta vez al mando del entonces CF. DIM. Marcelo García Apala , ya con los sistemas de propulsión instalados vuelve a botarse la embarcación procediéndose a las pruebas de maquinas y realizándose la primera travesía rumbo al Puerto de Guaqui, donde proseguirían los trabajos de construcción del salón, cubierta al aire libre y el puente de mando, esta decisión tomada por el mando naval, se debió a las características favorables de este puerto, aguas tranquilas y poco viento, ideales para terminar la obra.</p>
+    <p align="justify"> El año 2006 el CN. DIM. Marcelo García Apala logró cambiar el nombre del proyecto a “Construcción del Buque Multipropósito” con la visión de que el buque tendría que ser autosostenible y tener varias tareas de impacto socio cultural para fortalecer la imagen de la Institución Naval. Paralelamente el mismo CN. García impulsó la creación del Servicio de Industria Naval a partir del proyecto, pero con la intención de desarrollar la construcción de buques a nivel nacional.</p>
+    <div class="imagen_paquetes">
+        <img src="/images/us101.jpg" />
+    </div>
+    <p align="justify"> Durante las gestiones 2006 y 2007 por orden del entonces V. Almte. José Alba Arnés Comandante de la F.N.B. se paralizó el proyecto congelando la asignación de recursos  económicos y frustrando una aspiración institucional por un lapso de tiempo valioso.</p>
+    <p align="justify"> A mediados del año 2007 el Director de la obra CN. Marcelo García Apala con el apoyo de autoridades locales de Guaqui logró captar el interés del Presidente del Estado Evo Morales y se logra un apoyo económico de 411000 $U$ a través del programa “Evo Cumple”, con este presupuesto se decide de manera acertada reemplazar los motores propulsores adquiridos a medio uso por Motores Scania de ultima tecnología sueca y con el presupuesto restante se logra concluir la obra el 12 de febrero de 2009.</p>
+    <p align="justify"> A partir de la entrega de la obra asume el CC. CGON. Adalid Alfaro Palma como el Primer Comandante del Buque Multipropósito, quien fue componente importante del equipo de constructores en el área de propulsión y maquinas navales, con la misión de dar continuidad al proyecto en cuanto a detalles técnicos y de operacionalidad de la embarcación, además de lograr que la unidad sea autosostenible para no erogar gastos al Estado.</p>
+    <div class="imagen_acerc">
+        <img src="/images/us100.jpg" />
+    </div>
+    <p align="justify"> A partir de entonces el “Buque Multipropósito” empezó a desarrollar diversas tareas como el apoyo a la capacitación del personal de cuadros, cadetes, alumnos y marineros en el arte de la navegación, apoyo a Defensa Civil ante desastres naturales en poblaciones ribereñas, apoyo a las actividades de investigación científica para el cuidado del medio ambiente y arqueología subacuática y la tarea mas complicada, desarrollar el turismo en la región de Guaqui.   </p>
+    <p align="justify"> Durante la gestión 2010 el Buque Multipropósito con dependencia del Departamento III Operaciones, logró cumplir las tareas encomendadas y en el ámbito turístico solo en su primer año de funcionamiento acogió a más de 10000 turistas en su mayoría nacionales realizando cruceros a través del lago Titicaca contribuyendo de manera exitosa al desarrollo regional y logrando la autosostenibilidad del proyecto.  </p>
+    <div class="imagen_acerc">
+        <img src="/images/us102.jpg" />
+    </div>
+    <p align="justify"> A fines de 2010  el Buque Multipropósito se convierte en la Unidad Operativa de Servicios “NAVTUR” asumiendo como primer Comandante el CC. CGON. Adalid Alfaro Palma  con la misión de dar consecución a los logros obtenidos, teniendo a su cargo el Buque Multipropósito, tres embarcaciones menores, un Astillero de mantenimiento y dos oficinas de información en la ciudad de La Paz.</p>
+    <p align="justify"> El año 2013 pasa a depender de la Dirección General de Participación y Apoyo al Desarrollo conformándose las Unidades dependientes en base a las Unidades Operativas de Servicios como Transnaval, Industria Naval, Agropecuaria encarando actividades y proyectos orientados a desarrollar los II.MM.FF. y LL. en Bolivia. </p>
+    <p align="justify"> En la actualidad esta unidad tiene la proyección de convertirse en una unidad a nivel nacional con Buques Multipropósito y subunidades de apoyo en localidades estratégicas como Trinidad, Ribereralta, Guayaramerin, Quijarro, para de esta manera multiplicar los impactos económicos, socioculturales y educativos que realiza esta unidad en beneficio institucional y de la población boliviana.   </p>
     <h3>Función que cumple en cuanto a seguridad, defensa y desarrollo</h3>
     <div class="imagen_paquetes">
-            <img src="/images/us103.jpg" />
-        </div>
+        <img src="/images/us103.jpg" />
+    </div>
     <p align="justify"> La U.O.S. “NAVTUR”, utilizando el concepto multipropósito, cuenta con unidades multipropósito, como el BM-01 preposicionados en el sector sur del Lago Titicaca con capacidad operativa y altamente flexibles como la lancha Karachi anticontrabando, realizando acciones disuasivas en defensa de nuestras fronteras con el vecino país del Perú, en los sectores de Desaguadero, Taraco, Guaqui y otros.</p>
     <h3>Impacto de los resultados en la sociedad</h3>
     <div class="imagen_paquetes">
-            <img src="/images/us104.jpg" />
-        </div>
+        <img src="/images/us104.jpg" />
+    </div>
     <p align="justify"> Desde su inicio el año 2010 hasta la fecha el BM-01 dependiente de la U.O.S. “NAVTUR” ha recibido a 47000 turistas, mas de 200 unidades educativas, 28000 niños y jóvenes que recibieron charlas y realizaron actividades marineras fortaleciendo la conciencia marítima. 8000 turistas extranjeros que conocieron la belleza de nuestro paisaje andino y el Lago Titicaca. </p>
     <p align="justify"> El éxito alcanzado contribuyendo al turismo en el sector sur del Lago Titicaca, tuvo sus efectos positivos en la Localidad de Guaqui, donde sus autoridades locales impulsaron la construcción de dos museos “El museo del Fierro” y el “Museo del Altiplano” así como el impulso para atraer más turistas y fieles a la Iglesia del “Apóstol Santiago”, concretándose, el año 2011 un convenio para consolidar el “Circuito Ferroviario Lacustre”, entre la Armada Boliviana, la Alcaldía de Guaqui y el Tren Turística de la Cruz Andina. Producto de estos logros, en la actualidad se están encarando proyectos para mejorar la oferta turística en Guaqui para consolidar a esta población como un punto turístico reconocido oficialmente.</p>
     <p align="justify"> Diversas instituciones y organismos del Estado Plurinacional de Bolivia, como la Asamblea Legislativa, el Ministerio de Educación, Ministerio de Turismo y Culturas, Ministerio de Defensa, Comando en Jefe de las FF.AA., el Comando General de la Armada Boliviana, y otros, que realizaron diversas actividades institucionales, de investigación científica, reuniones de trabajo, contribuyendo de esta manera a la gestión del Estado Plurinacional de Bolivia. </p>
--- apps/frontend/modules/home/templates/crucerosSuccess.php
+++ apps/frontend/modules/home/templates/crucerosSuccess.php
@@ -1,39 +1,128 @@
 <div class ="servicios">
-    
-    <div class="article">
-      <h4><span>Cruceros </span> Sociales</h4>
-        <ul type="circle">
-            <li> Matrimonios </li>
-            <li> 15 Años </li>
-            <li> Cumpleaños </li>
-        </ul>
-     
-    </div>
-    
-    <div class="article">
-       
-        <h4><span>Cruceros </span> Corporativos</h4>
-        <ul type="circle">
-            <li> Seminarios </li>
-            <li> Conferencias </li>
-            <li> Simposios </li>
-        </ul>
-    </div>
-    <div class="article">
-        <h4><span>Cruceros </span> Educativos</h4>
-        <ul type="circle">
-            <li> Viajes Educativos </li>
-            <li> Viajes y Fiestas de Promoción </li>
-            <li> Simposios </li>
-        </ul>
-    </div>
-    <div class="article"><h4><span>Cruceros </span> Ferroviario Lacustre</h4></div>
-    <div class="article"><h4><span>Cruceros </span> de Confraternidad</h4></div>
-    <div class="article"><h4><span>Cruceros </span> de Semana Santa</h4></div>
-    <div class="article"><h4><span>Cruceros </span> de Año Nuevo</h4></div>
-    <div class="article"><h4><span>Cruceros </span> para Papá</h4></div>
-    <div class="article"><h4><span>Cruceros </span> para Mamá</h4></div>
+    <table >
+        <tr>
+            <td style="width: 60%">
+                <h4><span>Cruceros </span> Sociales</h4>
+                <ul type="circle">
 
+                    <li> Matrimonios </li>
+                    <li> 15 Años </li>
+                    <li> Cumpleaños </li>
+                </ul>
+        <li><?php echo link_to('Leer más acerca del crucero social', 'home/crucerosSociales') ?></li>
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/matrimonio2.jpg"/>
+            </div>
+        </td>
+
+        </tr>
+        <tr>
+            <td>
+                <h4><span>Cruceros </span> Corporativos</h4>
+                <ul type="circle">
+                    <li> Seminarios </li>
+                    <li> Conferencias </li>
+                    <li> Simposios </li>
+                </ul>
+        <li><?php echo link_to('Leer más acerca del crucero corporativo', 'home/crucerosCorporativos') ?></li>
+        </td>
+        <td>
+            <div class="imagen_acerc">
+                <img src="/images/seminario1.jpg"/>
+            </div>
+        </td>
+        </td>
+        </tr>
+        <tr>
+            <td>
+                <h4><span>Cruceros </span> Educativos</h4>
+                <ul type="circle">
+                    <li> Viajes Educativos </li>
+                    <li> Viajes y Fiestas de Promoción </li>
+                    <li> Simposios </li>
+                </ul>
+        <li><?php echo link_to('Leer más acerca del crucero educativo simple', 'home/cruceroEducativo') ?></li>
+        <li><?php echo link_to('Leer más acerca del crucero educativo completo', 'home/cruceroEducativoTodo') ?></li>
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/edu1.jpg"/>
+            </div>
+        </td>
+        </tr>
+        <tr>
+            <td> 
+                <h4><span>Cruceros </span> Ferroviario Lacustre</h4>
+        <li><?php echo link_to('Leer más acerca del crucero ferroviario lacustre', 'home/cruceroFerroviarioLac') ?></li>
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/ferrov1.jpg"/>
+            </div>
+        </td>
+        </tr>
+        <tr>
+            <td>  
+                <h4><span>Cruceros </span> de Confraternidad</h4>
+        <li><?php echo link_to('Leer más acerca del crucero de confraternidad simple', 'home/cruceroConfraternidad') ?></li>
+        <li><?php echo link_to('Leer más acerca del crucero de confraternidad completo', 'home/cruceroConfraternidadTodo') ?></li>
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/confra3.jpg"/>
+            </div>
+        </td>
+        </tr>
+        <tr>
+            <td> 
+                <h4><span>Cruceros </span> de Semana Santa</h4>
+        <li><?php echo link_to('Leer más acerca del crucero de semana santa', 'home/semanaSanta') ?></li>
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/confra4.jpg"/>
+            </div>
+        </td>
+        </tr>
+        <tr>
+            <td> 
+                <h4><span>Cruceros </span> de Año Nuevo</h4>
+        <li><?php echo link_to('Leer más acerca del crucero de año nuevo', 'home/anoNuevo') ?></li>
+
+        </td>
+        <td>
+            <div class="imagen_acerc">
+                <img src="/images/anonuevo.jpg"/>
+            </div>
+        </td>
+        </tr>
+        <tr>
+            <td> 
+                <h4><span>Cruceros </span> para Papá</h4>
+        <li><?php echo link_to('Leer más acerca del crucero para papá', 'home/cruceroPapa') ?></li>
+
+
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/parapapa1.jpg"/>
+            </div>
+        </td>
+        </tr>
+        <tr>
+            <td> 
+                <h4><span>Cruceros </span> para Mamá</h4>
+        <li><?php echo link_to('Leer más acerca del crucero para mamá', 'home/cruceroMama') ?></li>
+        </td>
+        <td>
+            <div class="imagen_cruc">
+                <img src="/images/paramama.jpg"/>
+            </div>
+        </td>
+        </tr>
+    </table>     
     <br>
 
 </div>
