e72851c5dd37b72fe9ba734f0d3d392239695bb5
rickyos <ricardo_barronb@hotmail.com>
2014-09-11 17:32:32 -0400

    se cambiaron el tamaño de las imagenes y se agregaron unas imagenes en la parte de acerca de

--- apps/frontend/modules/home/templates/crucerosSuccess.php
+++ apps/frontend/modules/home/templates/crucerosSuccess.php
@@ -1,29 +1,39 @@
 <div class ="servicios">
-
-    <h4><span>Cruceros </span> Sociales</h4>
-    <ul type="circle">
-        <li> Matrimonios </li>
-        <li> 15 Años </li>
-        <li> Cumpleaños </li>
-    </ul>
-    <h4><span>Cruceros </span> Corporativos</h4>
-    <ul type="circle">
-        <li> Seminarios </li>
-        <li> Conferencias </li>
-        <li> Simposios </li>
-    </ul>
-    <h4><span>Cruceros </span> Educativos</h4>
-    <ul type="circle">
-        <li> Viajes Educativos </li>
-        <li> Viajes y Fiestas de Promoción </li>
-        <li> Simposios </li>
-    </ul>
-    <h4><span>Cruceros </span> Ferroviario Lacustre</h4>
-    <h4><span>Cruceros </span> de Confraternidad</h4>
-    <h4><span>Cruceros </span> de Semana Santa</h4>
-    <h4><span>Cruceros </span> de Año Nuevo</h4>
-    <h4><span>Cruceros </span> para Papá</h4>
-    <h4><span>Cruceros </span> para Mamá</h4>
-    <br>
     
+    <div class="article">
+      <h4><span>Cruceros </span> Sociales</h4>
+        <ul type="circle">
+            <li> Matrimonios </li>
+            <li> 15 Años </li>
+            <li> Cumpleaños </li>
+        </ul>
+     
+    </div>
+    
+    <div class="article">
+       
+        <h4><span>Cruceros </span> Corporativos</h4>
+        <ul type="circle">
+            <li> Seminarios </li>
+            <li> Conferencias </li>
+            <li> Simposios </li>
+        </ul>
+    </div>
+    <div class="article">
+        <h4><span>Cruceros </span> Educativos</h4>
+        <ul type="circle">
+            <li> Viajes Educativos </li>
+            <li> Viajes y Fiestas de Promoción </li>
+            <li> Simposios </li>
+        </ul>
+    </div>
+    <div class="article"><h4><span>Cruceros </span> Ferroviario Lacustre</h4></div>
+    <div class="article"><h4><span>Cruceros </span> de Confraternidad</h4></div>
+    <div class="article"><h4><span>Cruceros </span> de Semana Santa</h4></div>
+    <div class="article"><h4><span>Cruceros </span> de Año Nuevo</h4></div>
+    <div class="article"><h4><span>Cruceros </span> para Papá</h4></div>
+    <div class="article"><h4><span>Cruceros </span> para Mamá</h4></div>
+
+    <br>
+
 </div>
